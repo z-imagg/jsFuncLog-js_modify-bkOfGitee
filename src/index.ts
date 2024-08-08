de3b52f0cfe6b4562fc3e130ac98d2e5997a16e4
prgrmz07 <prgrmz07@163.com>
2024-08-08 16:49:19 +0800

    ts-morph修改x.ts例子

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-// 参考: https://juejin.cn/post/7213277254183845945
+// 参考: https://gist.github.com/banyudu/cf5a6c8ff4b6c8acec97a5517c0fa583
 // ts-ast在线解析  https://ts-ast-viewer.com/
 
-import { Project,SourceFile,ClassDeclaration,FunctionDeclaration,ImportDeclaration,VariableDeclaration } from "ts-morph";
+import { Project,SourceFile,ClassDeclaration,FunctionDeclaration,ImportDeclaration,VariableDeclaration ,SyntaxKind,KindToNodeMappings} from "ts-morph";
 
 // 创建一个TypeScript项目对象
 const project:Project = new Project();
 
-// 从文件系统加载tsconfig.json文件，并将其中的所有源文件添加到项目中
-// project.addSourceFilesAtPaths("/app2/WebCola/tsconfig.json");
 project.addSourceFilesAtPaths("/app2/WebCola/WebCola/**/*.ts");
 
 
@@ -15,26 +13,24 @@ project.addSourceFilesAtPaths("/app2/WebCola/WebCola/**/*.ts");
 const sourceFiles:SourceFile[] = project.getSourceFiles();
 console.log(`sourceFiles=${sourceFiles}`)
 
-// 获取所有类名
-const classNames:string[] = sourceFiles.flatMap((sourceFile:SourceFile) =>
-  sourceFile.getClasses().map((classDecl:ClassDeclaration) => classDecl.getName())
-);
-console.log(`classNames=${classNames}`)
+for (const srcFile of sourceFiles) {
+  const actions: Array<() => void> = []
+  // 获取源文件srcFile中第一个变量声明
+  const singleNode:KindToNodeMappings[SyntaxKind.VariableDeclaration]|undefined = srcFile.getFirstDescendantByKind(SyntaxKind.VariableDeclaration)
 
-// 获取所有函数名
-const functionNames = sourceFiles.flatMap((sourceFile:SourceFile) =>
-  sourceFile.getFunctions().map((funcDecl:FunctionDeclaration) => funcDecl.getName())
-);
-console.log(`functionNames=${functionNames}`);
+  //若空则跳过
+  if(!singleNode){ continue;}
+  
+  // 把所有 修改动作 保存起来
+  actions.unshift(() => {
+    singleNode.replaceWithText('{ hello: 1, world: 2 }')
+  })
+  
+  // 最后 一并执行 修改动作
+  actions.forEach((act) => {
+    act()
+  })
+}
 
-// 获取所有import语句
-const importLs:ImportDeclaration[] = sourceFiles.flatMap((sourceFile:SourceFile) => sourceFile.getImportDeclarations());
-importLs.forEach((importDecl:ImportDeclaration,k:number) => {
-  console.log(`${k},importDecl,`,importDecl.getModuleSpecifierValue());
-});
-
-// 获取所有变量声明
-const variables:VariableDeclaration[] = sourceFiles.flatMap((sourceFile:SourceFile) => sourceFile.getVariableDeclarations());
-variables.forEach((variable:VariableDeclaration,k:number) => {
-  console.log(`${k},variable,`,variable.getName());
-});
+//保存项目
+project.saveSync()

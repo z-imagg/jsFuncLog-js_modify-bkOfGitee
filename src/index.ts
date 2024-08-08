550ae6683a9c4aea8c38f20ca657fca081c76721
prgrmz07 <prgrmz07@163.com>
2024-08-08 12:40:10 +0800

    esprima解析、修改js例子

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 //esm
-import {Parser,Program} from 'acorn'
+import * as esprima from 'esprima'
+import * as escodegen from 'escodegen'
+import * as ast_types from 'ast-types'
+import * as estree from 'estree'
 
 //commonjs
-// const {Parser} = require("acorn")
+// const esprima = require('esprima');
+// const escodegen = require('escodegen');
+// const b = require('ast-types').builders;
+const b = ast_types.builders;
 
-const program:Program=Parser.parse(`function func1(arg1,arg2){}`,{ecmaVersion:'latest',sourceType:'script'})
-console.log(`program=${JSON.stringify(program)}`)
+const program = `export default [
+  require('@/pages/page1/model').default,
+]`;
+const tree:esprima.Program = esprima.parseModule(program,{loc:true});
+
+const body:estree.ExportDefaultDeclaration=tree.body[0]  as estree.ExportDefaultDeclaration
+
+const elements:any[] =  (body.declaration as estree.ArrayExpression).elements
+
+const newNode:ast_types.namedTypes.MemberExpression = b.memberExpression(
+  b.callExpression(
+    b.identifier('require'), [
+      b.literal('@/pages/page2/model'),
+    ],
+  ),
+  b.identifier('default'),
+)
+elements.push(newNode);
+
+const newCode:string = escodegen.generate(tree);
+console.log(newCode);

e95e125eaef45cbfe76173c77cfba49ad15e85b6
prgrmz07 <prgrmz07@163.com>
2024-08-09 11:55:05 +0800

    [开发草稿] 变量更名

--- src/utils/CalcFuncName_of_MethodDecl.ts
+++ src/utils/CalcFuncName_of_MethodDecl.ts
@@ -2,14 +2,14 @@
 import { MethodDeclaration } from "ts-morph";
 import { getFuncStartLineNumAsFuncName } from "./Basic";
 
-export function calcFuncName_of_MethodDecl(funcDecl:MethodDeclaration){
-  if(!funcDecl){ throw new Error("此函数入参不应该为空,calcFuncName_of_MethodDecl")}
+export function calcFuncName_of_MethodDecl(methodDecl:MethodDeclaration){
+  if(!methodDecl){ throw new Error("此函数入参不应该为空,calcFuncName_of_MethodDecl")}
 
-  const funcName_byLineNum:string=getFuncStartLineNumAsFuncName(funcDecl);
+  const funcName_byLineNum:string=getFuncStartLineNumAsFuncName(methodDecl);
   //无可奈何时,返回用行号构造的函数名
-  if(funcDecl.getChildCount()<=0){ return funcName_byLineNum;}
+  if(methodDecl.getChildCount()<=0){ return funcName_byLineNum;}
 
-  const field=funcDecl.getChildAtIndex(0);
+  const field=methodDecl.getChildAtIndex(0);
   //以字段名为函数名
   return  field.getText();
 }//end_func calcFuncName_of_FuncExpr

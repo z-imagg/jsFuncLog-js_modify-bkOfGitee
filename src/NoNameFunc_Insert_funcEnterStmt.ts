1e1d66b14107dfd51caab32e701495b3fdaea6ec
prgrmz07 <prgrmz07@163.com>
2024-08-09 11:04:11 +0800

    [代码整理中] 提取范型2

--- src/NoNameFunc_Insert_funcEnterStmt.ts
+++ src/NoNameFunc_Insert_funcEnterStmt.ts
@@ -3,9 +3,12 @@
 // 参考: https://gist.github.com/banyudu/cf5a6c8ff4b6c8acec97a5517c0fa583
 // ts-ast在线解析  https://ts-ast-viewer.com/
 
-import { Project,SourceFile,SyntaxKind,Statement,FunctionExpression,MethodDeclaration} from "ts-morph";
+import { Project,SourceFile,SyntaxKind,Statement,FunctionExpression, KindToNodeMappings} from "ts-morph";
 import { calcFuncName_of_FuncExpr } from "./utils/GetFuncName";
 import { get_firstStmt_of_FunctionExpression } from "./func_process/FunctionExpression_Process";
+
+//调用入口函数
+main_insertFuncEnterStmt()
 
 //执行修改动作 所需要的材料
 interface ModifyMaterial{
@@ -13,7 +16,8 @@ interface ModifyMaterial{
   stmt0:Statement|undefined
   //函数名
   funcName:string
-}
+}//end_interface ModifyMaterial
+
 //执行修改动作
 function execModifyAction(mat:ModifyMaterial){
   //跳过无第一条语句的函数
@@ -22,7 +26,7 @@ function execModifyAction(mat:ModifyMaterial){
   const 函数进入语句文本:string=`  const _funcName/* :string */='${mat.funcName}' ; _funcNoArgs_enter_log(_srcFilePath,_funcName ) ; `
   //在函数第一条语句前添加 日志语句
   mat.stmt0.replaceWithText(`${函数进入语句文本} ; ${mat.stmt0.getText()}`)
-}
+}//end_func execModifyAction
 
 function calcFuncName_of_Func<T_FuncDecl>(funcDecl:T_FuncDecl, funcDeclTypeEnum:SyntaxKind):string|undefined{
   if(funcDeclTypeEnum==SyntaxKind.FunctionExpression){
@@ -32,7 +36,8 @@ function calcFuncName_of_Func<T_FuncDecl>(funcDecl:T_FuncDecl, funcDeclTypeEnum:
     // return calcFuncName_of_MethodDecl(funcDecl as MethodDeclaration)
   }
   return undefined;
-}
+}//end_func calcFuncName_of_Func
+
 function get_firstStmt_of_Func<T_FuncDecl>(funcDecl:T_FuncDecl, funcDeclTypeEnum:SyntaxKind):Statement|undefined{
   if(funcDeclTypeEnum==SyntaxKind.FunctionExpression){
     return get_firstStmt_of_FunctionExpression(funcDecl as FunctionExpression)
@@ -41,7 +46,10 @@ function get_firstStmt_of_Func<T_FuncDecl>(funcDecl:T_FuncDecl, funcDeclTypeEnum
     // return get_firstStmt_of_MethodDecl(funcDecl as MethodDeclaration)
   }
   return undefined;
-}
+}//end_func get_firstStmt_of_Func
+
+//入口函数, 插入函数进入语句
+function main_insertFuncEnterStmt(){
 const focuse_srcF_prefix:string="/app2/WebCola/WebCola/src";
 // 创建一个TypeScript项目对象
 const project:Project = new Project();
@@ -61,20 +69,39 @@ for (const srcFile of sourceFiles) {
 
   console.log(`文件名,fileBaseName=${fileBaseName}, filePath=${filePath}`)
 
+  const materialLs:Array<ModifyMaterial>|undefined =  srcFile_process<FunctionExpression,SyntaxKind.FunctionExpression>(srcFile,SyntaxKind.FunctionExpression)
+  //跳过空返回
+  if(!materialLs || materialLs.length == 0 ){ continue;}
+
+  // 最后 一并执行 修改动作
+  materialLs.forEach((mat)=>execModifyAction(mat))
+
+}//end_for 源文件遍历
+
+//保存项目
+project.saveSync()
+}//end_func main函数
+
+
+//FMD==Function or Method Declare
+function srcFile_process<T_FMD,SK_FMD extends SyntaxKind>(srcFile:SourceFile,  funcDeclTypeEnum:SK_FMD):Array<ModifyMaterial>|undefined{
+//SK_FMD 举例 SyntaxKind.FunctionExpression
+//ts这里有点怪味道,  SyntaxKind.FunctionExpression 是 enum SyntaxKind 的一个具体整数值, 但 又可以说 SyntaxKind.FunctionExpression extends SyntaxKind ? 
+
   const materialLs:Array<ModifyMaterial> = []
 
   // 获取源文件srcFile中匿名函数声明,   具体可观看 https://ts-ast-viewer.com/  解释x.ts 结果中的 FunctionExpression
-  const funcDeclLs:FunctionExpression[] = srcFile.getDescendantsOfKind(SyntaxKind.FunctionExpression)
+  const funcDeclLs: KindToNodeMappings[SK_FMD][] /* FunctionExpression[] */ = srcFile.getDescendantsOfKind (funcDeclTypeEnum)//funcDeclTypeEnum比如 SyntaxKind.FunctionExpression
 
   //若空则跳过
-  if(!funcDeclLs || funcDeclLs.length == 0 ){ continue;}
+  if(!funcDeclLs || funcDeclLs.length == 0 ){ return undefined;}
   
   // 把所有 修改动作 保存起来
   //遍历匿名函数声明
   for(const funcDecl of funcDeclLs){
     //取函数名
-    let funcName:string =calcFuncName_of_Func<FunctionExpression>(funcDecl,SyntaxKind.FunctionExpression)
-    const stmt0:Statement|undefined=get_firstStmt_of_Func<FunctionExpression>(funcDecl,SyntaxKind.FunctionExpression)
+    let funcName:string =calcFuncName_of_Func<T_FMD>(funcDecl as T_FMD,funcDeclTypeEnum)
+    const stmt0:Statement|undefined=get_firstStmt_of_Func<T_FMD>(funcDecl as T_FMD,funcDeclTypeEnum)
     //忽略起止行号相同的函数,忽略无函数体的函数,忽略无语句的函数,忽略第一条语句为空的函数
     console.log(`funcName=${funcName},起止行号 ${funcDecl.getStartLineNumber()}:${funcDecl.getEndLineNumber()},忽略么?${stmt0==undefined}`)
     if(!stmt0){ continue;}
@@ -84,11 +111,6 @@ for (const srcFile of sourceFiles) {
     materialLs.unshift(<ModifyMaterial>{funcName,stmt0})
 
   }//end_for 函数声明遍历
-  
-  // 最后 一并执行 修改动作
-  materialLs.forEach((mat)=>execModifyAction(mat))
 
-}//end_for 源文件遍历
-
-//保存项目
-project.saveSync()
+  return materialLs;
+}//end_func srcFile_process
